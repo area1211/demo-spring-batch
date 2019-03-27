729572b2ea65eed87085dc0bf0691b2250a6d31b
Jung Inho <area1211@naver.com>
2019-03-27 23:43:19 +0900

    3-2. BATCH_JOB_EXECUTION 완료

--- src/main/java/com/example/demospringbatch/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/demospringbatch/job/SimpleJobConfiguration.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,15 +22,31 @@ public class SimpleJobConfiguration {
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob") // simpleJob 이라는 이름으로 Batch Job을 생성한다.
-                .start(simpleStep1()) // Spring Batch에서 Job은 하나의 배치 작업 단위를 말한다.
+                .start(simpleStep1(null)) // Spring Batch에서 Job은 하나의 배치 작업 단위를 말한다.
+                .next(simpleStep2(null))
                 .build();
     }
 
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}")String requestDate) {
         return stepBuilderFactory.get("simpleStep1") // BatchStep 생성
                 .tasklet((contribution, chunkContext) -> { // tasklet은 Step 안에서 단일로 수행될 커스텀한 기능들을 선언할 때 사용한다.
+//                    throw new IllegalArgumentException("step1에서 실패합니다.");
                     log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}")String requestDate) {
+        return stepBuilderFactory.get("simpleStep2") // BatchStep 생성
+                .tasklet((contribution, chunkContext) -> { // tasklet은 Step 안에서 단일로 수행될 커스텀한 기능들을 선언할 때 사용한다.
+                    log.info(">>>>> This is Step2");
+                    log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
